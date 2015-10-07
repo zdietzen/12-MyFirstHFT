7a55bdbd9286bf1e429c8ac2c27f2d3542a6d906
zdietzen <diezel37@gmail.com>
2015-10-07 16:57:31 -0700

    Second Commit
    This is nowhere near done. I need to add the timer and get it to compare
    the to stock prices.

---
+++ 12-MyFirstHFT/Classes/Stock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12_MyFirstHFT.Classes
+{
+    class Stock
+    {
+        public string ask { get; set; }
+        public string symbol { get; set; }
+    }
+}
--- 12-MyFirstHFT/Program.cs
+++ 12-MyFirstHFT/Program.cs
@@ -1,24 +1,28 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using _12_MyFirstHFT.Classes;
+using Newtonsoft.Json.Linq;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _12_MyFirstHFT
 {
     class Program
     {
+        private static decimal pastAsk; 
+
         static void Main(string[] args)
         {
             Console.WriteLine("Please input your stocks ticker symbol and press Enter");
-
+            // this saves the user input to a string
             string ticker = Console.ReadLine();
 
-            using (WebClient webClient = new WebClient())
-            {
+            //this is the timer
+            var timer = new System.Threading.Timer(e => stock, null, TimeSpan.Zero.TimeSpan.FromSeconds(3));
 
+
+            using (WebClient webClient = new WebClient())
+
+            {
+                Stock stock = new Stock();
                 //Says Hello to Yahoo Finance and tells them we aren't that weird 
                 webClient.Headers.Add("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)");
 
@@ -28,21 +32,19 @@ namespace _12_MyFirstHFT
                 //this converts the json info into usable information for us
                 var a = JArray.Parse(json);
 
-
-
-
-
-
-
-                //This saves the json information into a string
-                string symbol = a[0]["symbol"].ToString();
-                string ask = a[0]["ask"].ToString();
-
+               //This saves the json information into a string
+                stock.symbol = a[0]["symbol"].ToString();
+                stock.ask = a[0]["ask"].ToString();
                 
 
-                //Writes info to console
-                Console.WriteLine(ask);
-                Console.ReadLine();
+                pastAsk = Convert.ToDecimal(stock.ask);
+
+                
+                    //Writes info to console
+                    Console.WriteLine("The current price of " + ticker + " is " + stock.ask);
+
+                    Console.ReadLine();
+                }
             }
         }
     }
